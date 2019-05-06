7f578e4b42fd144c7110ac4a8ef41a473bb7310e
bradbread <48189304+bradbread@users.noreply.github.com>
2019-05-06 15:38:16 +1000

    basic dice rolling program created

---
+++ DiceRolling/DiceRolling/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRolling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool play = true;
+            string option;
+            int min, max, numR;
+            dice d = new dice();
+            
+
+            Console.WriteLine("Welcome to the Caves & Lizards Club dice rolling program");
+            Console.WriteLine("Press r to roll the dice");
+            Console.WriteLine("Press x to roll a custom dice multiple times");
+            Console.WriteLine("Press d to roll a custom dice");
+            Console.WriteLine("Press a to get the average of your rolls");
+            Console.WriteLine("Press t to get the total of your rolls");
+            Console.WriteLine("Press h to get your rolls");
+            Console.WriteLine("Press c to clear your rolls");
+            Console.WriteLine("Press q to quit");
+            while (play) {
+                Console.WriteLine("Please select an option");
+                option = Console.ReadLine();
+                switch (option) {
+                    case "r":
+                        d.RollDice();
+                        break;
+                    case "d":
+                        Console.WriteLine("please enter the min then the max number of the dice");
+                        min = Convert.ToInt32(Console.ReadLine());
+                        max = Convert.ToInt32(Console.ReadLine()) + 1;
+                        d.RollDice(min, max);
+                        break;
+                    case "x":
+                        Console.WriteLine("please enter the number of rolls you wish to perform");
+                        numR = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("please enter the min then the max number of the dice");
+                        min = Convert.ToInt32(Console.ReadLine());
+                        max = Convert.ToInt32(Console.ReadLine()) + 1;
+                        for (int i = 0; i < numR; i++) {
+                            d.RollDice(min, max);
+                        }
+                        break;
+                    case "a":
+                        Console.WriteLine("please enter the number of rolls you wish to average");
+                        max = Convert.ToInt32(Console.ReadLine());
+                        d.PrintAverage(max);
+                        break;
+                    case "t":
+                        Console.WriteLine("please enter the number of rolls you wish to add to the total");
+                        max = Convert.ToInt32(Console.ReadLine());
+                        d.PrintTotal(max);
+                        break;
+                    case "h":
+                        Console.WriteLine("please enter the number of rolls you wish to display");
+                        max = Convert.ToInt32(Console.ReadLine());
+                        d.PrintRolls(max);
+                        break;
+                    case "c":
+                        d.ClearRolls();
+                        break;
+                    case "q":
+                        play = false;
+                        break;
+                    default:
+                        Console.WriteLine("invalid input");
+                        break;
+                }
+            }
+
+            Console.WriteLine("exiting program press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DiceRolling/DiceRolling/dice.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRolling
+{
+    class dice
+    {
+        List<int> rolls = new List<int>();
+        Random rnd = new Random();
+
+        public void RollDice(int min = 1, int max = 7) {
+            rolls.Add(rnd.Next(min, max));
+            Console.WriteLine("you rolled a " + rolls.Last());
+        }
+
+        //should have made these without the console.writeline and just made them return an int with the average/total so i could reuse the code in total by calling it in the average method
+        public void PrintAverage(int length) {
+            if (length <= rolls.Capacity)
+            {
+
+                int sum = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    sum += rolls[i];
+                }
+                sum /= length;
+                Console.WriteLine("the average of your dice rolls was " + sum);
+            } else {
+                Console.WriteLine("your entered number exceedes the number of rolls the average of all rolls is " + rolls.Average());
+            }
+        }
+
+        public void PrintTotal(int length) {
+
+            if (length <= rolls.Capacity) {
+
+                int sum = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    sum += rolls[i];
+                }
+                Console.WriteLine("the total of your dice rolls was " + sum);
+            } else {
+                Console.WriteLine("your entered number exceedes the number of rolls the total of all rolls is " + rolls.Sum());
+            }
+        }
+
+        public void PrintRolls(int length) {
+            if (length <= rolls.Capacity)
+            {
+
+                Console.WriteLine("you rolled");
+                for (int i = 0; i < length; i++)
+                {
+                    Console.WriteLine(rolls[i]);
+                }
+            } else {
+                Console.WriteLine("your entered number exceedes the number of rolls");
+            }
+        }
+
+        public void ClearRolls() {
+            rolls.Clear();
+            Console.WriteLine("your rolls have been cleared");
+        }
+
+    }
+}
